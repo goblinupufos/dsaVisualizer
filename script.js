4047dc40772e570ceaf72d01b2542228ffbf719b
goblinupufos <popescu.traian2003@gmail.com>
2023-02-07 18:34:31 +0100

    js script for adding boxes

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 "use strict";
 
-const maze = document.querySelector(".maze-container");
+const maze = document.querySelector(".maze");
 
 // create a function that creates the div boxes with a custom class
 // create a function that calculates the width of the maze and the number of divs that i can fit in it
 
-const mazeWidth = maze.offsetWidth;
-
-function createBox() {
+function createBox(number) {
 	const box = document.createElement("div");
 	box.classList.add("box");
-
+	// box.textContent = `${number}`;
+	// box.textContent = `.`;
+	// console.log(number);
 	return box;
 }
 
-for (let i = 0; i <= 713; ++i) {
-	maze.append(createBox());
+function renderMaze() {
+	const mazeWidth = maze.offsetWidth;
+	const mazeHeight = maze.offsetHeight;
+	const boxesPerRow = Math.floor(mazeWidth / 26);
+	const boxesPerCol = Math.floor(mazeHeight / 26);
+	console.log(mazeWidth, mazeHeight);
+	console.log(boxesPerRow, boxesPerCol);
+
+	for (let row = 1; row <= boxesPerCol; ++row) {
+		for (let col = 1; col <= boxesPerRow; ++col) {
+			maze.append(createBox(col));
+		}
+	}
 }
+renderMaze();
